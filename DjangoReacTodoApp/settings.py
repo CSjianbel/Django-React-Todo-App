c6924c6740efb65053cc0abe54ea3804a26333d9
OjouNii <belarminojiankarlo@gmail.com>
2022-06-04 13:04:25 +0800

    React integration completed - Utilized webpack and babel

--- DjangoReacTodoApp/settings.py
+++ DjangoReacTodoApp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'react.apps.ReactConfig',
+    'frontend.apps.FrontendConfig',
     'rest_framework'
 ]
 
